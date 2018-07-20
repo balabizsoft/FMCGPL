6b572fe988f98fae804fd76829096898c021ffb4
PravinaR <pravina.bizsoft@gmail.com>
2018-07-20 13:03:37 +0530

    Starting Connection

--- src/app/Services/app-lib.service.ts
+++ src/app/Services/app-lib.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { CompanyDetail } from '../Models/CompanyDetail';
+import { UserAccount } from '../Models/UserAccount';
+import { UserType } from '../Models/UserType';
+import { SignalR, SignalRConnection } from 'ng2-signalr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppLibService {
-
-  constructor() { }
+companyDetailList: CompanyDetail[];
+userAccountList: UserAccount[];
+userTypeList: UserType[];
+ con: SignalRConnection;
+  constructor(private s1: SignalR) {
+this.con = this.s1.createConnection();
+console.log(this.con.status);
+this.con.start().then(x => {
+console.log(x);
+this.con.invoke('').then(cd => {
+  console.log(cd);
+  this.companyDetailList = cd;
+});
+this.con.invoke('').then(ua => {
+  console.log(ua);
+  this.userAccountList = ua;
+});
+this.con.invoke('').then(ut => {
+  console.log(ut);
+  this.userTypeList = ut;
+});
+});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {SignalRModule, SignalRConfiguration} from 'ng2-signalr';
+
+export function createConfig(): SignalRConfiguration {
+const c = new SignalRConfiguration;
+c.hubName = '';
+c.url = '';
+c.logging = true;
+
+return c;
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +20,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SignalRModule.forRoot(createConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
